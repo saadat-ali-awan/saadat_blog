492ecb7bbc87ed1d0e72ee336a769b48e01a62b4
Saadat Ali <saadatlucky313@gmail.com>
2022-06-08 04:06:18 +0500

    Create Users Home Page and Show Users Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  get "/users/:id",to: "users#show"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "users#index"
 end
