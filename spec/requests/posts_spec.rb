bf88946450fbf8791e8e3b9edfd49af995cad4d7
Saadat Ali <saadatlucky313@gmail.com>
2022-06-10 06:53:07 +0500

    test correct placeholder text

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,14 +1,16 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe 'PostsController', type: :request do
   it 'when post page found' do
     get '/users/12/posts'
     expect(response).to have_http_status(200)
-    expect(response).to render_template("index")
+    expect(response).to render_template('index')
+    expect(response.body).to include('Show all posts for a user...')
   end
   it 'when post opened' do
     get '/users/12/posts/13'
     expect(response).to have_http_status(200)
-    expect(response).to render_template("show")
+    expect(response).to render_template('show')
+    expect(response.body).to include('Show a post for a user...')
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -5,11 +5,13 @@ describe 'Users Controller' do
     get '/'
     expect(response).to have_http_status(200)
     expect(response).to render_template('index')
+    expect(response.body).to include('Hello! This is All Users List...')
   end
 
   it 'open user detail' do
     get '/users/12'
     expect(response).to have_http_status(200)
     expect(response).to render_template('show')
+    expect(response.body).to include('Show A User...')
   end
 end
