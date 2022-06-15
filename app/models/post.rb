e95a09c0b4d638882fbe4b0e2de4acf10ebe50e0
Saadat Ali <saadatlucky313@gmail.com>
2022-06-16 04:39:27 +0500

    Add method to change post counter

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,13 @@
 class Post < ApplicationRecord
   has_many :comments
   has_many :likes
-  belongs_to :user
+  belongs_to :author, class_name: 'User', foreign_key: 'user_id'
+
+  def increment_user_post_counter
+    user.increment!(:post_counter)
+  end
+
+  def decrement_user_post_counter
+    user.decrement!(:post_counter)
+  end
 end
