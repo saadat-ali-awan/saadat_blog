5b32680ec8d805006e722c7179ef3aed159dbe23
Saadat Ali <saadatlucky313@gmail.com>
2022-06-16 04:39:36 +0500

    Add method to change comment counter

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,12 @@
 class Comment < ApplicationRecord
-  belongs_to :user
+  belongs_to :author, class_name: 'User', foreign_key: 'user_id'
   belongs_to :post
+
+  def increment_post_comments_counter
+    post.increment!(:comment_counter)
+  end
+
+  def decrement_post_comments_counter
+    post.decrement!(:comment_counter)
+  end
 end
