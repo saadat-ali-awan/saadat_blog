d5dc9192a23fc9a6b97569681e2764a78bf67258
Saadat Ali <saadatlucky313@gmail.com>
2022-06-18 02:14:53 +0500

    Handle Post Action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,4 +11,50 @@ class PostsController < ApplicationController
     post_id = params['id']
     @post = Post.find(post_id)
   end
+
+  def new
+    post = Post.new
+    respond_to do |format|
+      format.html { render :new, locals: { post: post } }
+    end
+  end
+
+  def create
+    new_post = Post.new(post_params)
+    errors = []
+    errors << "Title Missing From Post" if params[:post][:title] == ''
+    errors << "Text Missing From Post" if params[:post][:text] == ''
+    unless errors.length.zero?
+      errors.each do |error|
+        if flash.now[:danger].nil?
+          flash.now[:danger] = [ error ]
+        else
+          flash.now[:danger] << error
+        end
+      end
+      respond_to do |format|
+        format.html do
+          render :new, locals: {post: new_post}, status: 422
+        end
+      end
+      
+      return
+    end
+    new_post.author = current_user
+    respond_to do |format|
+      format.html do
+        if new_post.save
+          flash[:success] = [ "Post Created Successfully" ]
+          redirect_to posts_url
+        else
+          flash.now[:danger] = [ "Error: Post could not be saved" ]
+          render :new, locals: {post: new_post}, status: 422
+        end
+      end
+    end
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
