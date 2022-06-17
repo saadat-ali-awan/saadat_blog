576a17bd937246b15603214f0fb2b385e1a722e5
Saadat Ali <saadatlucky313@gmail.com>
2022-06-18 02:13:39 +0500

    Add Routes For adding new post

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get "/users/:id", to: "users#show"
-  get "/users/:user_id/posts", to: "posts#index"
+  get "/users/:user_id/posts", to: "posts#index", as: 'posts'
+  get "/users/:user_id/posts/new", to: "posts#new", as: "new_post"
+  post "users/:user_id/posts", to: "posts#create"
   get "users/:user_id/posts/:id", to: "posts#show"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
