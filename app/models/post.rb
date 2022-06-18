f86031392083b2aa8813bb290c01ef0d52478e91
Saadat Ali <saadatlucky313@gmail.com>
2022-06-18 10:16:36 +0500

    Add validations and fix decrement counter

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,11 +3,20 @@ class Post < ApplicationRecord
   has_many :likes
   belongs_to :author, class_name: 'User', foreign_key: 'user_id'
 
+  validates_associated :author
+
+  validates :title, presence: true, length: { in: 1..250 }
+  validates :text, presence: true, length: { in: 1..250 }
+  validates :comment_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0}
+  validates :like_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0}
+
   def increment_user_post_counter
     author.increment!(:post_counter)
   end
 
   def decrement_user_post_counter
-    author.decrement!(:post_counter)
+    unless author.post_counter.zero?
+      author.decrement!(:post_counter)
+    end
   end
 end
