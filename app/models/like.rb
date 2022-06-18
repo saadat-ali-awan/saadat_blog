459c2c1c89de0e70aca314a8072cf793c822bdeb
Saadat Ali <saadatlucky313@gmail.com>
2022-06-18 10:16:16 +0500

    Add Validation and fix decrement counter

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,11 +2,16 @@ class Like < ApplicationRecord
   belongs_to :post
   belongs_to :author, class_name: 'User', foreign_key: 'user_id'
 
+  validates_associated :post
+  validates_associated :author
+
   def increment_post_likes_counter
     post.increment!(:like_counter)
   end
 
   def decrement_post_likes_counter
-    post.decrement!(:like_counter)
+    unless post.like_counter.zero?
+      post.decrement!(:like_counter)
+    end
   end
 end
