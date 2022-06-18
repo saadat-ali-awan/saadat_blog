5be0ae5d709411f440a815412c65a877d16e3e3e
Saadat Ali <saadatlucky313@gmail.com>
2022-06-18 05:14:55 +0500

    Add route for comment and like

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   get "/users/:id", to: "users#show"
   get "/users/:user_id/posts", to: "posts#index", as: 'posts'
   get "/users/:user_id/posts/new", to: "posts#new", as: "new_post"
+  post "/users/:user_id/posts/:id/commment", to: "comments#comment", as: "new_comment" 
+  post "/users/:user_id/posts/:id/like", to: "likes#like", as: "new_like"
   post "users/:user_id/posts", to: "posts#create"
-  get "users/:user_id/posts/:id", to: "posts#show"
+  get "users/:user_id/posts/:id", to: "posts#show", as: "user_post"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
