f3ad986258f3961ca81498940a311ab9e7edb1ef
Saadat Ali <saadatlucky313@gmail.com>
2022-06-23 03:53:50 +0500

    Change default value of post_counter to be 0

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_22_210018) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_22_223228) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -48,7 +48,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_22_210018) do
     t.string "name"
     t.string "photo"
     t.string "bio"
-    t.integer "post_counter"
+    t.integer "post_counter", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
