bc8a1a45be843aaaceb62732fe157a6360edd6ea
Saadat Ali <saadatlucky313@gmail.com>
2022-06-23 05:31:55 +0500

    Add bio and picture during registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,6 @@ class ApplicationController < ActionController::Base
 
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name photo bio])
   end
 end
