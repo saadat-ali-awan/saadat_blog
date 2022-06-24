810a6248106aac59da5b0e01cfe6137e2aee892f
Saadat Ali <saadatlucky313@gmail.com>
2022-06-24 17:14:39 +0500

    Rescue from AccessDenied error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,9 @@ class ApplicationController < ActionController::Base
 
   add_flash_types :danger, :info, :warning, :success, :messages
 
+  rescue_from CanCan::AccessDenied do | exception |
+    redirect_to root_url, alert: exception.message
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[name photo bio])
