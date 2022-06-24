9998853cf999621ac642644a9bbddc8967e4d7ac
Saadat Ali <saadatlucky313@gmail.com>
2022-06-24 17:08:58 +0500

    Run new migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_22_223228) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_23_222317) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,8 +36,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_22_223228) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "text"
-    t.integer "comment_counter"
-    t.integer "like_counter"
+    t.integer "comment_counter", default: 0
+    t.integer "like_counter", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
@@ -60,6 +60,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_22_223228) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
+    t.string "role", default: "default"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
